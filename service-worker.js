4d7ae0141d6478230c66072af217a08f78f86c72
smob4real <146184661+smob4real@users.noreply.github.com>
2026-02-14 18:42:53 +0000

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = "wba-cache-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
